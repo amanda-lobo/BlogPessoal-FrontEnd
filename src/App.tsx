2a34097fe57d145f5e2b09689154038ac03ccfc7
Amanda Lobo <amandalobo.ag@gmail.com>
2022-05-25 22:25:42 -0300

    Adicionando Footer e Navbar na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Home from './pages/Home/Home';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import './App.css';
+
 
 
 function App() {
   return (
-    <Home />
+    <>
+     <Navbar />
+     <Home />
+     <Footer />
+    </>
   );
 }
 
